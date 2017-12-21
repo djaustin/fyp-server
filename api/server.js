e8fa35cd3670e31e579d6be352a516433d42117f
Dan Austin <dja470@bham.ac.uk>
2017-12-21 16:25:32 +0000

    Add express foundations to server.js to create basic running site

--- api/server.js
+++ api/server.js
@@ -1,2 +1,31 @@
-console.log("HELLO THERE");
-exec('ls -al')
+/*
+* IMPORTS
+*/
+
+// Web framework
+const express = require('express');
+// HTML template engine
+const pug = require('pug');
+// Logging tool for requests
+const morgan = require('morgan');
+// Initialise an express application
+const app = express();
+
+
+
+/*
+* APPLICATION CONFIGURATION
+*/
+// Set pug as the view engine for this express app
+app.set('view engine', 'pug');
+
+// Use the 'dev' template of logging for all requests to the app
+app.use(morgan('dev'));
+
+// Controller for route '<hostname>/'
+app.get('/', function(req, res){
+  res.render('index');
+})
+
+app.listen(80);
+console.log('Listening on port 80');
