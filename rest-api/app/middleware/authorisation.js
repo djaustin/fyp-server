84d981bad85fe005cfd8ce7179b348e923ea1497
Dan Austin <dja470@bham.ac.uk>
2018-01-02 09:14:45 +0000

    Add authorisation middleware to check authenticated userid against url id

---
+++ rest-api/app/middleware/authorisation.js
@@ -0,0 +1,22 @@
+exports.authenticatedUserOwnsResource = function(paramIdName){
+  return function(req, res, next){
+    //TODO: Error handling here
+    let user;
+    // This is different depending on whether authentication used basic or bearer
+    if(req.user.user){
+      user = req.user.user;
+    } else if(req.user) {
+      user = req.user;
+    }
+
+    const authenticatedId = user._id;
+    const requestId = req.params[paramIdName];
+    console.log(authenticatedId, requestId);
+    if(authenticatedId != requestId){
+      res.status(403);
+      res.json({message: 'Authenticated user is not authorized for that operation.'});
+    } else {
+      next();
+    }
+  }
+};
--- rest-api/app/routes/api/users/index.js
+++ rest-api/app/routes/api/users/index.js
@@ -8,14 +8,18 @@ const router = require('express').Router({mergeParams: true});
 const authentication = require('app/controllers/authentication');
 const userController = require('app/controllers/user');
 const usageLogRouter = require('app/routes/api/users/usage-logs');
+const authorisation = require('app/middleware/authorisation');
 
+const userIdMatchesAuthenticatedUser = authorisation.authenticatedUserOwnsResource('userId');
 // POST .../users/
 // Used to add a user to the system
 router.post('/', userController.newUser);
-router.get('/:userId', authentication.isUserAuthenticated, userController.getUser);
-//NOTE: ONLY USED TO TEST AUTH. REMOVE FOR PROD.
+// TODO: Is this needed
+router.get('/:userId', authentication.isUserAuthenticated, userIdMatchesAuthenticatedUser, userController.getUser);
+//TODO: ONLY USED TO TEST AUTH. REMOVE FOR PROD.
 router.get('/', authentication.isUserAuthenticated, userController.allUsers);
 
-router.use('/:userId/usage-logs', authentication.isBearerAuthenticated, usageLogRouter);
+// Only allow client token authentication and ensure that the token is for the userId of the logs attempting to be accessed
+router.use('/:userId/usage-logs', authentication.isBearerAuthenticated, userIdMatchesAuthenticatedUser, usageLogRouter);
 
 module.exports = router;
