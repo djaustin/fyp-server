e281504ffce32bff34b195f1f224dd9ecc15916a
Dan Austin <dja470@bham.ac.uk>
2018-03-15 13:43:50 +0000

    update load tests

--- tests/artillery/artillery-functions.js
+++ tests/artillery/artillery-functions.js
@@ -9,7 +9,19 @@ exports.generateFromAndToTimeLast7Days = function(requestParams, context, ee, ne
 
 
 exports.generateStartAndEndTime = function(requestParams, context, ee, next) {
-  context.vars.startTime = moment().subtract(10, 'seconds').valueOf()
+  context.vars.startTime = moment().subtract(1, 'minute').valueOf()
   context.vars.endTime = moment().valueOf()
   next()
+}
+
+exports.generateExceptionStartAndEndTime = function(requestParams, context, ee, next) {
+  var startMoment = moment().add(getRndInteger(0, 10), 'minutes')
+  var endMoment = startMoment.add(getRndInteger(0, 10), 'minutes')
+  context.vars.exceptionStartTime = startMoment.valueOf()
+  context.vars.exceptionEndTime = endMoment.valueOf()
+  next()
+}
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
